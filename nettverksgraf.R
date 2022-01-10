f5af0dcd06cc47d592db7630de29fc4458c2035f
Christian Lura <christian.lura@nrk.no>
2022-01-10 11:09:21 +0100

    Update nettverksgraf.R
    Skript som lager nettverksgraf basert på tre kolonner med noder i Excel.

--- nettverksgraf.R
+++ nettverksgraf.R
@@ -5,7 +5,7 @@ library(htmlwidgets)
 
 nwdata <- read_xlsx("nettverksdata.xlsx")
 
-#Lag en gruppe basert p� kolonne 3 i dataene
+#Lag en gruppe basert på kolonne 3 i dataene
 make.group <- function(x) {
   group <- subset(nwdata, nwdata[,3] == x)
   group <- unique(unlist(group[,1]))
@@ -17,21 +17,21 @@ d3 <- data.frame(from = nwdata[,1], to = nwdata[,2])
 
 #Enkelt plot
 nwplot <- simpleNetwork(d3, height="200px", width="200px",        
-                   Source = 1,                 # column number of source
-                   Target = 2,                 # column number of target
-                   linkDistance = 10,          # distance between node. Increase this value to have more space between nodes
-                   charge = -900,                # numeric value indicating either the strength of the node repulsion (negative value) or attraction (positive value)
-                   fontSize = 14,               # size of the node names
-                   fontFamily = "serif",       # font og node names
-                   linkColour = "#666",        # colour of edges, MUST be a common colour for the whole graph
-                   nodeColour = "#69b3a2",     # colour of nodes, MUST be a common colour for the whole graph
-                   opacity = 0.9,              # opacity of nodes. 0=transparent. 1=no transparency
-                   zoom = T                    # Can you zoom on the figure?
+                   Source = 1,                 # kolonnenummer til source
+                   Target = 2,                 # kolonnenummer til target
+                   linkDistance = 10,          # distanse mellom noder. Øk verdien for å lage mer rom mellom nodene
+                   charge = -900,              # numerisk verdi som indikerer enten nodenes frastøtning (negativ verdi) eller tiltrekning (positiv verdi)
+                   fontSize = 14,              # størrelse på nodenavnene
+                   fontFamily = "serif",       # font til nodenavn
+                   linkColour = "#666",        # farge på kanter, må være felles for hele grafen
+                   nodeColour = "#69b3a2",     # farge på noder, være felles for hele grafen
+                   opacity = 0.9,              # Nodens transparensgrad. 0 = gjennomsiktig, 1 = ingen transparens opacity of nodes. 0=transparent. 1=no transparency
+                   zoom = T                    # Ja eller nei til zoome-mulighet
 )
 
 saveWidget(nwplot, file="nwplot_enkel.html")
 
-#Avansert - med fargekoding p� grupper
+#Avansert - med fargekoding på grupper
 
 fra <- data.frame(nwdata[,1])
 til <- data.frame(nwdata[,2])
@@ -44,7 +44,7 @@ nodefactors <- as.factor(sort(unique(nodefactors)))
 noder <- data.frame(name=nodefactors, group=1, size=15)
 
 #Grupper i datasettet kan defineres som en vil. I dette prosjektet het de
-#Kontakter (standardgruppe), Investorer, Hongkong, N�r familie, Sverige.
+#Kontakter (standardgruppe), Investorer, Hongkong, Nær familie, Sverige.
 inv <- make.group("inv")
 hk <- make.group("hk")
 familie <- make.group("fam")
@@ -56,7 +56,7 @@ sverige.color <- match(sverige, levels(nodefactors))
 noder[,2] <- "Kontakter"
 noder[inv.color,2] <- "Investorer"
 noder[hk.color,2] <- "Hongkong"
-noder[familie.color,2] <- "N�r familie"
+noder[familie.color,2] <- "Nær familie"
 noder[sverige.color,2] <- "Sverige"
 
 source <- match(fra, levels(nodefactors)) - 1
@@ -70,7 +70,7 @@ nwplot2 <- forceNetwork(Links = links,
                   Target = "Target",
                   Group = "group",
                   NodeID = "name",
-                  charge = -600, # node repulsion
+                  charge = -600, 
                   linkDistance = 100,
                   zoom = T, 
                   opacity = 0.9,
